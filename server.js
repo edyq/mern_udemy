86c44f7a48363b16b43143d0b49cccd39d13b47d
ewigkeit <15670318223@163.com>
2020-07-31 10:25:50 +0800

    create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// bring up the express server and let it run
+
+const express = require('express');
+
+const app = express();
+
+// create endpoint for connection testing
+app.get('/', (req, res) => res.send('API Running'));
+
+// create the env var to set PORT. when we deploy it on heroku, it will look
+// for the env var to connect to PORT 
+// if there is no env var set, it will go to 5000
+const PORT = process.env.PORT || 5000;
+
+// create callback. log when connected
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
