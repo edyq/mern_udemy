e5c50cfeacaa6e99f4ee95ae1fece3989a2e98e3
YT <wouktb@outlook.com>
2020-08-11 21:24:11 +0800

    40. auth reducer and register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,12 +2,13 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import { axios } from 'axios';
 import PropTypes from 'prop-types'
 
 
 // what the fuck is props
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   // formData: the object that stores the data of the form
   // setFormData: the function that handels the state of the form
   const [formData, setFormData] = useState({
@@ -26,23 +27,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Password do not match', 'danger');
     } else {
-      const newUser = {
-        name,
-        email,
-        password,
-      }
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-        const body = JSON.stringify(newUser);
-        const res = await axios.post('/api/users', body, config);
-        console.log(res.data);
-      } catch (error) {
-        console.error(error.response.data);
-      }
+      register({ name, email, password });
     }
   }
   return (
@@ -57,7 +42,7 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required />
+          />
         </div>
         <div className="form-group">
           <input
@@ -66,12 +51,11 @@ const Register = ({ setAlert }) => {
             name="email"
             value={email}
             onChange={e => onChange(e)}
-            required
           />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+            Gravatar email
+          </small>
         </div>
         <div className="form-group">
           <input
@@ -80,8 +64,6 @@ const Register = ({ setAlert }) => {
             name="password"
             value={password}
             onChange={e => onChange(e)}
-            required
-            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -91,8 +73,6 @@ const Register = ({ setAlert }) => {
             name="password2"
             value={password2}
             onChange={e => onChange(e)}
-            required
-            minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -106,7 +86,8 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
